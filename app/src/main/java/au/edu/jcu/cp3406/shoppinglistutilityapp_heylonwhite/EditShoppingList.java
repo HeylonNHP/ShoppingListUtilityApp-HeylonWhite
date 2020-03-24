6f763d6fec8323010751fddc38f0ca1df5e16d96
HeylonNHP <heylon96@hotmail.com>
2020-03-24 19:09:20 +1000

    Created main activity

---
+++ app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/EditShoppingList.java
@@ -0,0 +1,14 @@
+package au.edu.jcu.cp3406.shoppinglistutilityapp_heylonwhite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_shopping_list);
+    }
+}
--- app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/MainActivity.java
@@ -3,6 +3,8 @@ package au.edu.jcu.cp3406.shoppinglistutilityapp_heylonwhite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.main_listview, R.id.textView, new String[]{"one","two"} );
+        ListView listView = (ListView)findViewById(R.id.shopping_list_view);
+        listView.setAdapter(adapter);
     }
 }
