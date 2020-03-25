67590e8a7d6c37be0a995968c2eb70652d0cdca3
HeylonNHP <heylon96@hotmail.com>
2020-03-25 19:34:00 +1000

    Created edit shopping list activity

--- app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/MainActivity.java
@@ -2,9 +2,13 @@ package au.edu.jcu.cp3406.shoppinglistutilityapp_heylonwhite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.main_listview, R.id.textView, new String[]{"one","two"} );
+        ArrayList<String> list = new ArrayList<>();
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.main_listview, R.id.textView, list );
         ListView listView = (ListView)findViewById(R.id.shopping_list_view);
         listView.setAdapter(adapter);
+
+        list.add("shit");
+    }
+
+    public void editShoppingListClick(View view){
+        Intent intent = new Intent(this,EditShoppingList.class);
+        startActivity(intent);
     }
 }
