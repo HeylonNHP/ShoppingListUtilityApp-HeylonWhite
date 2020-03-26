9d9ea4c9f2c8227b7a970475e4c60db08fcbe2ea
HeylonNHP <heylon96@hotmail.com>
2020-03-26 16:01:14 +1000

    Set layout weights for lists and added home button

--- app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/EditShoppingList.java
+++ app/src/main/java/au/edu/jcu/cp3406/shoppinglistutilityapp_heylonwhite/EditShoppingList.java
@@ -3,12 +3,35 @@ package au.edu.jcu.cp3406.shoppinglistutilityapp_heylonwhite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class EditShoppingList extends AppCompatActivity {
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter adapter;
+    EditText input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_shopping_list);
+
+        input = (EditText)findViewById(R.id.textView3);
+
+
+        adapter = new ArrayAdapter<String>(this,R.layout.main_listview, R.id.textView, list );
+        ListView listView = (ListView)findViewById(R.id.shoppingListView);
+        listView.setAdapter(adapter);
+
+    }
+
+    public void addItemClick(View view) {
+        list.add(input.getText().toString());
+        adapter.notifyDataSetChanged();
     }
 }
